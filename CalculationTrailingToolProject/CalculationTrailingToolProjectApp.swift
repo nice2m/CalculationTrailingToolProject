2d819001b2bf1f0258c3ce52232391c613fbc4a5
ntm <ntm_xyz@163.com>
2023-03-04 16:29:58 +0800

    Initial Commit

---
+++ CalculationTrailingToolProject/CalculationTrailingToolProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculationTrailingToolProjectApp.swift
+//  CalculationTrailingToolProject
+//
+//  Created by nice2meet on 2023/3/4.
+//
+
+import SwiftUI
+
+@main
+struct CalculationTrailingToolProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculationTrailingToolProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalculationTrailingToolProject
+//
+//  Created by nice2meet on 2023/3/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
